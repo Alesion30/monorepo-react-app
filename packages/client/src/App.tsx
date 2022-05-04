0152840fb20d20946970ceee8bb844c079f31d3b
Yuta Ohira <alesion90@gmail.com>
2022-05-04 15:17:38 +0900

    🎨 format

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,17 +1,18 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img alt="logo" className="App-logo" src={logo} />
         <p>Hello Vite + React!</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+          <button onClick={() => setCount((count) => count + 1)} type="button">
             count is: {count}
           </button>
         </p>
@@ -22,24 +23,24 @@ function App() {
           <a
             className="App-link"
             href="https://reactjs.org"
-            target="_blank"
             rel="noopener noreferrer"
+            target="_blank"
           >
             Learn React
           </a>
-          {' | '}
+          {" | "}
           <a
             className="App-link"
             href="https://vitejs.dev/guide/features.html"
-            target="_blank"
             rel="noopener noreferrer"
+            target="_blank"
           >
             Vite Docs
           </a>
         </p>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
